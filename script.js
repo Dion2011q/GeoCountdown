ce146315fce17b0d5934344657481d9fcd2ef214
Martijn <mdrodermond@icloud.com>
2026-01-13 10:27:21 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 function updateCountdown() {
-	const targetDate = new Date('2026-1-1T00:00:01')
+	const targetDate = new Date('2025-12-31T23:59:59')
   const now = new Date();
 	let diff = targetdate - now;
   const countdownEl = document.getElementById("countdown");
